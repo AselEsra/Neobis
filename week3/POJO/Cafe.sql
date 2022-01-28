9f625bc2fa2f60c7fe91e37829b4bc9acaff85a9
Asel Esra Ozyilmaz <esra.ozyilmaz@iaau.edu.kg>
2022-01-28 21:59:40 +0600

    cafe database

---
+++ week3/POJO/Cafe.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE Cafe;
+USE Cafe;
+
+CREATE TABLE Cake (
+    ID int NOT NULL,
+	Flavour nvarchar(50),
+    Frosting nvarchar(50),
+    Price int,
+	PRIMARY KEY (ID)
+); 
+
+CREATE TABLE Drink (
+    ID int NOT NULL,
+	[Type] nvarchar(50) NOT NULL,
+	Sugar bit,
+    Temperature nvarchar(50),
+    Price int,
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE Customer (
+    ID int NOT NULL,
+	FirstName nvarchar(50),
+    LastName nvarchar(50) NOT NULL,
+    Address nvarchar(255),
+	PhoneNumber int,
+	PRIMARY KEY (ID)
+);
